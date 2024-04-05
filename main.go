9cb541eb6ff54ba9ffca05755e8e176364edb6f1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-05 19:19:24 +0530

    REST API Using go-gin and DynamoDB

--- main.go
+++ main.go
@@ -1,65 +1,159 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+    "fmt"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
+type Item struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
+}
+
+var svc *dynamodb.DynamoDB
+
 func main() {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		fmt.Println("Error loading AWS config:", err)
-		return
-	}
+    sess, err := session.NewSession(&aws.Config{
+        Endpoint: aws.String("http://localhost:8000"),
+    })
+    if err != nil {
+        panic(err)
+    }
 
-	client := dynamodb.NewFromConfig(cfg)
+    svc = dynamodb.New(sess)
 
-	tableName := "MyTable"
+    router := gin.Default()
 
-	err = putItem(client, tableName)
-	if err != nil {
-		fmt.Println("Error putting item:", err)
-		return
-	}
+    router.POST("/items", createItemHandler)
+    router.GET("/items/:id", getItemHandler)
+    router.PUT("/items/:id", updateItemHandler)
+    router.DELETE("/items/:id", deleteItemHandler)
 
-	item, err := getItem(client, tableName)
-	if err != nil {
-		fmt.Println("Error getting item:", err)
-		return
-	}
-	fmt.Println("Item Retrieved:", item)
+    router.Run(":8080")
 }
 
-func putItem(client *dynamodb.Client, tableName string) error {
-	input := &dynamodb.PutItemInput{
-		TableName: aws.String(tableName),
-		Item: map[string]types.AttributeValue{
-			"ID": &types.AttributeValueMemberS{Value: "1"},
-			"Name": &types.AttributeValueMemberS{Value: "Virat Kohli"},
-		},
-	}
+func createItemHandler(c *gin.Context) {
+    var item Item
+    if err := c.BindJSON(&item); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
 
-	_, err := client.PutItem(context.TODO(), input)
-	return err
+    err := putItem(item)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusCreated, item)
 }
 
-func getItem(client *dynamodb.Client, tableName string) (map[string]types.AttributeValue, error) {
-	input := &dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]types.AttributeValue{
-			"ID": &types.AttributeValueMemberS{Value: "1"},
-		},
-	}
+func getItemHandler(c *gin.Context) {
+    id := c.Param("id")
 
-	output, err := client.GetItem(context.TODO(), input)
-	if err != nil {
-		return nil, err
-	}
+    item, err := getItem(id)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
 
-	return output.Item, nil
+    if item == nil {
+        c.Status(http.StatusNotFound)
+        return
+    }
+
+    c.JSON(http.StatusOK, item)
+}
+
+func updateItemHandler(c *gin.Context) {
+    id := c.Param("id")
+
+    var item Item
+    if err := c.BindJSON(&item); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    item.ID = id
+    err := putItem(item)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, item)
+}
+
+func deleteItemHandler(c *gin.Context) {
+    id := c.Param("id")
+
+    err := deleteItem(id)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.Status(http.StatusNoContent)
+}
+
+func putItem(item Item) error {
+    av, err := dynamodbattribute.MarshalMap(item)
+    if err != nil {
+        return err
+    }
+
+    input := &dynamodb.PutItemInput{
+        Item:      av,
+        TableName: aws.String("my-table-name"),
+    }
+
+    _, err = svc.PutItem(input)
+    return err
+}
+
+func getItem(id string) (*Item, error) {
+    input := &dynamodb.GetItemInput{
+        Key: map[string]*dynamodb.AttributeValue{
+            "id": {
+                S: aws.String(id),
+            },
+        },
+        TableName: aws.String("my-table-name"),
+    }
+
+    result, err := svc.GetItem(input)
+    if err != nil {
+        return nil, err
+    }
+
+    if len(result.Item) == 0 {
+        return nil, nil
+    }
+
+    var item Item
+    err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+    if err != nil {
+        return nil, err
+    }
+
+    return &item, nil
+}
+
+func deleteItem(id string) error {
+    input := &dynamodb.DeleteItemInput{
+        Key: map[string]*dynamodb.AttributeValue{
+            "id": {
+                S: aws.String(id),
+            },
+        },
+        TableName: aws.String("my-table-name"),
+    }
+
+    _, err := svc.DeleteItem(input)
+    return err
 }
